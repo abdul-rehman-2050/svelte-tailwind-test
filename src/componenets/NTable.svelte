008b64ff203618ff73a525586b9a514d6ca3ec3c
Abdul Rehman <supermind2002@gmail.com>
2023-02-05 12:24:00 +0500

    table with schema

---
+++ src/componenets/NTable.svelte
@@ -0,0 +1,30 @@
+<table>
+    <thead>
+      <tr>
+        {#each schema as column}
+          <th>{column.label}</th>
+        {/each}
+      </tr>
+    </thead>
+    <tbody>
+      {#each items as item}
+        <tr>
+          {#each schema as column}
+            <td>{item[column.key]}</td>
+          {/each}
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+  
+  <script>
+  /**
+	 * @type {any}
+	 */
+   export let items;
+  /**
+	 * @type {any}
+	 */
+   export let schema;
+  </script>
+  
--- src/routes/hello/+page.svelte
+++ src/routes/hello/+page.svelte
@@ -1,1 +1,39 @@
+<script>
+	import NTable from '../../componenets/NTable.svelte';
+</script>
+
 <h1>Simple Hello Route</h1>
+
+<NTable
+	items={[
+		{
+			createdAt: '2023-02-05T03:42:38.940Z',
+			name: 'Amber Jaskolski',
+			avatar:
+				'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/255.jpg',
+			email: 'Ethel.Stiedemann@yahoo.com',
+			phone: '1-759-582-6069',
+			address: 'Suite 669',
+			id: '1'
+		},
+		{
+			createdAt: '2023-02-04T19:30:32.708Z',
+			name: 'Lawrence Barrows',
+			avatar:
+				'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1073.jpg',
+			email: 'Blaise9@hotmail.com',
+			phone: '333-615-0721',
+			address: 'Suite 039',
+			id: '2'
+		}
+	]}
+	schema={[
+        { label: 'ID', key: 'id' },
+		{ label: 'Created At', key: 'createdAt' },
+		{ label: 'Name', key: 'name' },
+		{ label: 'Avatar', key: 'avatar' },
+		{ label: 'Email', key: 'email' },
+		{ label: 'Phone', key: 'phone' },
+		{ label: 'Address', key: 'address' }
+	]}
+/>
